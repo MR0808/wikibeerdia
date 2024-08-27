845647022cdd6b5d3363ff4f91e11a9c918a4d58
Mark <kram@grebnesor.com>
2024-08-27 06:44:50 +0000

    Register is working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["organic-space-rotary-phone-5r5w4qxq6qh7r9j-3000.app.github.dev", "localhost:3000"]
+      }
+    }
+  }
+  
 export default nextConfig;
+
