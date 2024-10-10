78bfae10d2773e6c30affe21f2835a9ace5ad6c7
MR0808 <kram@grebnesor.com>
2024-10-10 19:05:23 +1100

    Added autocomplete back

---
+++ components/autocomplete/Autocomplete.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+import { AddressType } from "@/types/autocomplete";
+import AddressAutoComplete from "./AddressAutoComplete";
+
+const Autocomplete = () => {
+    const [address, setAddress] = useState<AddressType>({
+        address1: "",
+        address2: "",
+        formattedAddress: "",
+        city: "",
+        region: "",
+        postalCode: "",
+        country: "",
+        lat: 0,
+        lng: 0,
+        countryCode: ""
+    });
+    const [searchInput, setSearchInput] = useState("");
+    return (
+        <AddressAutoComplete
+            address={address}
+            setAddress={setAddress}
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+        />
+    );
+};
+
+export default Autocomplete;
