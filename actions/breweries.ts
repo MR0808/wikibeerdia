d0118a0719734a373802f5d0f58ae921f4ef7235
MR0808 <kram@grebnesor.com>
2024-10-12 16:51:08 +1100

    Updated brewery action for creation

--- actions/breweries.ts
+++ actions/breweries.ts
@@ -2,12 +2,14 @@
 
 import * as z from "zod";
 import { Brewery } from "@prisma/client";
-import { zfd } from "zod-form-data";
+import { redirect } from "next/navigation";
 
 import db from "@/lib/db";
 import { currentUser } from "@/lib/auth";
-import { BrewerySchema, BrewerySchemaFormData } from "@/schemas/brewery";
+import { BrewerySchemaFormData } from "@/schemas/brewery";
+import { uploadImage, deleteImage } from "@/utils/supabase";
 import { getUserById } from "@/data/user";
+import { getErrorMessage } from "@/lib/handleError";
 
 export const createBrewery = async (formData: FormData) => {
     console.log("values", formData);
@@ -15,21 +17,92 @@ export const createBrewery = async (formData: FormData) => {
         const user = await currentUser();
 
         if (!user) {
-            return { result: false, message: "Unauthorized" };
+            return {
+                data: null,
+                error: getErrorMessage("Unauthorized")
+            };
         }
 
         const dbUser = await getUserById(user.id!);
 
         if (!dbUser) {
-            return { result: false, message: "Unauthorized" };
+            return {
+                data: null,
+                error: getErrorMessage("Unauthorized")
+            };
         }
 
         const form = BrewerySchemaFormData.safeParse(formData);
-        console.log(form);
 
-        // const logo = formData.get("logoUrl") as File;
+        if (form.error) {
+            return {
+                data: null,
+                error: getErrorMessage("Error with fields")
+            };
+        }
 
-        // console.log("logo", logo);
+        const {
+            name,
+            address1,
+            address2,
+            formattedAddress,
+            city,
+            region,
+            postalCode,
+            country,
+            countryCode,
+            description,
+            breweryType,
+            website,
+            logoUrl,
+            images
+        } = BrewerySchemaFormData.parse(formData);
+
+        const logoFullPath = await uploadImage(logoUrl[0], "logos-bucket");
+        let imagesUrls: string[] = [];
+        if (images && images?.length > 0) {
+            for (const image of images) {
+                const url = await uploadImage(image, "images-bucket");
+                imagesUrls.push(url);
+            }
+        }
+        const countryId = await db.country.findFirst({
+            where: { isoCode: countryCode }
+        });
+
+        if (!countryId) {
+            return {
+                data: null,
+                error: getErrorMessage("Error with fields")
+            };
+        }
+
+        const data = await db.brewery.create({
+            data: {
+                name,
+                address1,
+                address2,
+                formattedAddress,
+                city,
+                region,
+                postalCode,
+                countryId: countryId.id,
+                description,
+                breweryTypeId: breweryType,
+                website: website || "",
+                logoUrl: logoFullPath,
+                images: imagesUrls,
+                userId: dbUser.id
+            }
+        });
+        if (!data) {
+            return {
+                data: null,
+                error: getErrorMessage("Error with fields")
+            };
+        }
+
+        redirect(`/breweries/${data.id}`);
     } catch (error) {
         return renderError(error);
     }
