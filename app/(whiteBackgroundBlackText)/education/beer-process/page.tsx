65a6fe34ae9194e74d5766b0057365dad632ddbf
MR0808 <kram@grebnesor.com>
2025-01-03 22:52:47 +1100

    Continued process page

--- app/(whiteBackgroundBlackText)/education/beer-process/page.tsx
+++ app/(whiteBackgroundBlackText)/education/beer-process/page.tsx
@@ -5,9 +5,53 @@ import ProcessHero from "@/components/education/ProcessHero";
 
 const BeerProcessPage = () => {
     return (
-        <div className="bg-primary/15 pb-20">
-            <ProcessHero />
-        </div>
+        <>
+            <div className="bg-primary/15 pb-16">
+                <ProcessHero />
+            </div>
+            <div className="container mt-5 mb-10 flex flex-col justify-between pb-10 sm:justify-between sm:space-x-0 md:mt-10 md:pb-0">
+                <div className="flex flex-col justify-center md:flex-row">
+                    <div className="mb-10 text-center text-xl font-semibold italic md:hidden">
+                        "Beer isn't just made, it's created, it's a long
+                        process, created with love!"
+                    </div>
+                    <div className="flex w-full flex-row justify-center md:w-1/2">
+                        <Image
+                            src="/images/vat.png"
+                            alt="Beer styles"
+                            width={500}
+                            height={500}
+                            className="ease rounded-2xl object-cover object-center"
+                            sizes="(max-width: 640px) 100vw,(max-width: 1024px) 50vw, 33vw"
+                        />
+                    </div>
+                    <div className="mt-10 flex flex-col items-center justify-center justify-items-center space-y-10 md:mt-0 md:w-1/2">
+                        <div className="hidden text-center text-5xl font-semibold italic md:block">
+                            "Beer isn't just made, it's created, it's a long
+                            process, created with love!""
+                        </div>
+                        <div className="items-center space-y-5 text-left text-lg md:w-4/5">
+                            <p>
+                                Welcome to our extensive guide on how beer is
+                                made! Brewing beer is a captivating process that
+                                blends ancient traditions with modern
+                                innovations, creating a wide range of flavors,
+                                aromas, and styles. This intricate craft
+                                transforms simple ingredients into one of the
+                                world's most beloved beverages.
+                            </p>
+                            <p>
+                                Each step in the brewing process plays a vital
+                                role in shaping the unique characteristics of
+                                the final product. Dive into this detailed
+                                exploration of beer making, complete with
+                                imagery suggestions to visualize every stage.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 export default BeerProcessPage;
--- components/education/ProcessHero.tsx
+++ components/education/ProcessHero.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import { MoveRight, ArrowBigRight } from "lucide-react";
 
 import { averia } from "@/app/fonts";
 
 const ProcessHero = () => {
     return (
-        <div className="pb-5 drop-shadow-lg md:h-[550px] md:pb-0">
-            <div className="container flex flex-col pt-20 md:flex-row">
-                <div className="h-fit pt-2 pb-5 md:w-1/2 md:pt-10 md:pb-0">
+        <div className="h-full pb-2 drop-shadow-lg md:pb-0">
+            <div className="container flex flex-row justify-between space-x-5 pt-24">
+                <div className="h-fit pt-2 pb-5 md:pt-10 md:pb-0">
                     <Image
                         src="/images/ingredients.png"
                         width={400}
@@ -15,6 +16,37 @@ const ProcessHero = () => {
                         alt="Beer ingredients"
                     />
                 </div>
+                <div className="flex flex-col items-center pt-10 align-bottom">
+                    <Image
+                        src="/images/arrow.png"
+                        width={250}
+                        height={200}
+                        sizes="100vw"
+                        alt="Arrow"
+                        className="my-auto"
+                    />
+                </div>
+                <div className="h-fit pt-2 pb-5 md:pt-10 md:pb-0">
+                    <Image
+                        src="/images/finished-beer.png"
+                        width={400}
+                        height={400}
+                        sizes="100vw"
+                        alt="Beer ingredients"
+                    />
+                </div>
+            </div>
+            <div className="container mt-5 space-y-4">
+                <h1
+                    className={`${averia.className} text-center text-4xl leading-snug font-bold text-slate-600 uppercase drop-shadow-2xl sm:leading-snug md:text-6xl md:leading-[1.2]`}
+                >
+                    From hops and grains to the nectar of the gods
+                </h1>
+                <h2
+                    className={`${averia.className} text-center text-3xl leading-snug font-bold text-slate-600 uppercase drop-shadow-2xl sm:leading-snug md:text-4xl md:leading-[1.2]`}
+                >
+                    Let's explore how beer is made
+                </h2>
             </div>
         </div>
     );
