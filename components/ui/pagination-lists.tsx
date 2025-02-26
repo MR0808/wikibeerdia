bec1ac55eb7a32537ccc4a645d31817f9fafb29e
MR0808 <kram@grebnesor.com>
2025-02-27 00:37:53 +1100

    Hopefullylast time

--- components/ui/pagination-lists.tsx
+++ components/ui/pagination-lists.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { ButtonProps, buttonVariants } from "@/components/ui/button";
+import { buttonVariants, Button } from "@/components/ui/button";
 import {
     ChevronLeftIcon,
     ChevronRightIcon,
     DotsHorizontalIcon
 } from "@radix-ui/react-icons";
+
+type ButtonProps = React.ComponentProps<typeof Button>;
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
     <nav
